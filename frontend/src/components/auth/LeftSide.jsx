4094c407e0958690cf0ad7380921da3ac21b88dc
Muhammad Saleem <muhammadsaleem.webcoder@gmail.com>
2026-08-06 14:12:55 +0500

    Made login page fully responsive

--- frontend/src/components/auth/LeftSide.jsx
+++ frontend/src/components/auth/LeftSide.jsx
@@ -2,12 +2,17 @@ import React from "react";
 
 export default function LeftSide() {
   return (
-    <div className="w-[30%] sm:block hidden">
-      <div className="p-8">
-        <div className="flex gap-5">
-          <img src="/images/logo.png" alt="" className=" w-12" />
+    <div
+      className="w-[40%] sm:block md:hidden   hidden bg-cover bg-no-repeat bg-center h-screen items-center justify-center xl:block"
+      style={{
+        backgroundImage: "url('/images/leftSchool.png') ",
+      }}
+    >
+      <div className="px-8 py-2 z-10">
+        <div className="flex gap-5 items-center">
+          <img src="/images/logo.png" alt="" className=" w-16" />
           <div>
-            <h1 className="text-blue-600 text-lg font-bold">DZINNR SMS</h1>
+            <h1 className="text-amber-600 text-lg font-bold">DZINNR SMS</h1>
             <p className="text-gray-600 text-[12px]">
               School Management System
             </p>
@@ -16,7 +21,7 @@ export default function LeftSide() {
         <div className="mt-4 ">
           <h1 className="font-bold text-4xl">Smart School.</h1>
           <h1 className="font-bold text-4xl">
-            Better <span className="text-blue-600">Future.</span>
+            Better <span className="text-amber-600">Future.</span>
           </h1>
           <p className="mt-1">
             Manage students, teachers, classes, attendance, exams and more – all
@@ -24,21 +29,20 @@ export default function LeftSide() {
           </p>
         </div>
         <div>
-          <div className="flex items-center gap-4 mt-4">
-            <div className="bg-blue-50 p-4 rounded-full">
-              <i className="fa-regular fa-user text-blue-600 text-2xl"></i>
+          <div className="flex items-center gap-4 mt-5 bg-white w-[70%] p-2 rounded border border-zinc-400">
+            <div className="bg-amber-100 p-4 rounded-full">
+              <i className="fa-regular fa-user text-amber-600 text-2xl"></i>
             </div>
 
             <div>
-              {" "}
               <h4 className="font-bold">Role Based Access</h4>
               <p>Separate dashboards for Admin, Teachers and Students.</p>
             </div>
           </div>
 
-          <div className="flex items-center gap-4 mt-6">
-            <div className="bg-blue-50 p-4 rounded-full">
-              <i className="fa-solid fa-shield  text-blue-600 text-2xl"></i>
+          <div className="flex items-center gap-4 mt-2  bg-white w-[70%] p-2 rounded border border-zinc-400">
+            <div className="bg-amber-100 p-4 rounded-full">
+              <i className="fa-solid fa-shield  text-amber-600 text-2xl"></i>
             </div>
 
             <div>
@@ -46,9 +50,9 @@ export default function LeftSide() {
               <p>Your data is safe with us using modern authentication.</p>
             </div>
           </div>
-          <div className="flex items-center gap-4 mt-7">
-            <div className="bg-blue-50 p-4 rounded-full">
-              <i className="fa-solid fa-chart-simple  text-blue-600 text-2xl"></i>
+          <div className="flex items-center gap-4 mt-2  bg-white w-[70%] p-2 rounded border border-zinc-400">
+            <div className="bg-amber-100 p-4 rounded-full">
+              <i className="fa-solid fa-chart-simple  text-amber-600 text-2xl"></i>
             </div>
             <div>
               <h4 className="font-bold">Everything Organized</h4>
@@ -56,13 +60,6 @@ export default function LeftSide() {
             </div>
           </div>
         </div>
-      </div>
-      <div className="w-full h-92 bg-amber-300">
-        <img
-          src="/images/school.png"
-          alt=""
-          className="object-cover w-full h-full "
-        />
       </div>
     </div>
   );
--- frontend/src/pages/auth/Login.jsx
+++ frontend/src/pages/auth/Login.jsx
@@ -44,88 +44,100 @@ export default function Login() {
   };
 
   return (
-    <div className=" h-screen w-full flex">
+    <div className=" h-screen w-full flex  justify-center">
       <LeftSide />
-
-      <div className="w-[70%] p-5 flex flex-col items-center">
-        <div className="bg-blue-50 w-12 h-12 flex items-center justify-center rounded">
-          <i className="fa-solid fa-lock text-2xl text-blue-600"></i>
+      <div className="w-[85%] h-screen p-8 flex flex-col items-center sm:w-full sm:h-screen sm:justify-center ">
+        <div className="bg-amber-100 w-12 h-12 flex items-center justify-center rounded p-3 border border-amber-300 sm:block  sm:items-center sm:justify-center">
+          <i className="fa-solid fa-lock text-2xl text-amber-600"></i>
         </div>
         <div className="flex flex-col items-center ">
-          <h1 className="text-2xl font-bold mt-2">Welcome Back!</h1>
-          <p className="text-gray-800"> Log in to your account to continue</p>
+          <h1 className="text-3xl font-bold mt-2">Welcome Back!</h1>
+          <p className="text-gray-800 text-xl mt-2">
+            {" "}
+            Log in to your account to continue
+          </p>
         </div>
 
-        <form className="w-[90%] h-full " onSubmit={formSubmit}>
-          <div className="flex flex-col items-center gap-4 mt-3">
-            <p className="font-semibold">Select Your Role</p>
-            <div className="flex gap-5">
+        <form
+          className="w-full h-full sm:w-[80%] sm:h-full  "
+          onSubmit={formSubmit}
+        >
+          <div className="flex flex-col items-center gap-4 mt-3   ">
+            <p className="font-semibold mt-10">Select Your Role</p>
+            <div className="flex gap-5 w-full md:justify-center ">
               <button
                 type="button"
                 onClick={() => selectRoleUser("Admin")}
-                className={` ${selectedRole === "Admin" ? "border-blue-700" : "border-gray-300"} flex flex-col justify-center items-center bg-blue-50 rounded  border  w-42 h-32 gap-1`}
+                className={` ${selectedRole === "Admin" ? "bg-amber-50 border-amber-600" : "border-zinc-300"} flex flex-col justify-center items-center  rounded  border  w-42 h-32 gap-1`}
               >
-                <i className="fa-solid fa-user-tie text-3xl text-blue-600"></i>
+                <i className="fa-solid fa-user-tie text-3xl text-amber-600"></i>
                 <p className="font-bold">Admin</p>
                 <p>Full Access</p>
               </button>
               <button
                 type="button"
                 onClick={() => selectRoleUser("Teacher")}
-                className={` ${selectedRole === "Teacher" ? "border-green-700" : "border-gray-300"} flex flex-col justify-center items-center bg-blue-50 rounded  border  w-42 h-32 gap-1`}
+                className={` ${selectedRole === "Teacher" ? "bg-amber-50 border-amber-600" : "border-zinc-300"} flex flex-col justify-center items-center rounded  border  w-42 h-32 gap-1`}
               >
-                <i className="fa-solid fa-person-chalkboard text-3xl text-green-700"></i>
+                <i className="fa-solid fa-person-chalkboard text-3xl text-amber-700"></i>
                 <p className="font-bold">Teacher</p>
                 <p>Manage Classes</p>
               </button>
               <button
                 type="button"
                 onClick={() => selectRoleUser("Student")}
-                className={`${selectedRole == "Student" ? "border-purple-700" : "border-gray-300"} flex flex-col justify-center items-center bg-blue-50 rounded  border w-42 h-32 gap-1  `}
+                className={`${selectedRole == "Student" ? "bg-amber-50 border-amber-600" : "border-gray-300"}
+                 flex flex-col justify-center items-center  rounded  border w-42 h-32 gap-1  `}
               >
-                <i className="fa-solid fa-user-graduate text-3xl text-purple-700"></i>
+                <i className="fa-solid fa-user-graduate text-3xl text-amber-700"></i>
                 <p className="font-bold">Student</p>
                 <p>View & Learn</p>
               </button>
             </div>
-            <p className="text-green-700 font-semibold">
-              <i className="fa-regular fa-circle-check font-semibold"></i>{" "}
+            <p className="text-[#1A2332] font-semibold">
+              <i className="fa-regular fa-circle-check font-semibold text-amber-700"></i>{" "}
               {selectedRole} Role Selected
             </p>
           </div>
 
-          <div className=" h-24 mt-10 w-full">
+          <div className=" h-24 mt-10 w-full ">
             <label htmlFor="email" className="font-semibold">
               Email Address
             </label>{" "}
             <br />
-            <input
-              type="email"
-              name="email"
-              id="email"
-              className="w-full h-12 outline rounded px-4"
-              value={formData.email}
-              onChange={handleForm}
-            />
+            <div className="flex items-center justify-between  h-12 w-full border rounded mt-3">
+              <i className="fa-regular fa-envelope px-4"></i>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                className="w-full h-11 focus:outline-none rounded px-4"
+                value={formData.email}
+                onChange={handleForm}
+              />
+            </div>
           </div>
-          <div className=" h-24 mt-3">
+          <div className=" h-24 mt-6">
             <label htmlFor="password" className="font-semibold">
               Password
             </label>{" "}
             <br />
-            <input
-              type="password"
-              name="password"
-              id="password"
-              className="w-full h-12 outline rounded px-4"
-              value={formData.password}
-              onChange={handleForm}
-            />
+            <div className="w-full h-12 outline rounded px-4 flex items-center justify-between  gap-2 mt-3">
+              <i className="fa-solid fa-lock "></i>
+              <input
+                type="password"
+                name="password"
+                id="password"
+                className="w-full h-full   px-4 focus:outline-none"
+                value={formData.password}
+                onChange={handleForm}
+              />
+            </div>
           </div>
-          <div className="w-full">
+          <div className="w-full mt-12">
             <button
               type="submit"
-              className="bg-blue-600 text-white w-full h-12 rounded flex gap-2 justify-center items-center"
+              className="bg-amber-600 text-white w-full h-12 rounded flex gap-2 justify-center items-center"
             >
               <i className="fa-solid fa-right-to-bracket"></i>
               Login
@@ -138,6 +150,12 @@ export default function Login() {
               <p>{msg} </p>
             </div>
           )}
+        </p>
+        <p className="font-medium">
+          Don't have an account ?{" "}
+          <span className="text-amber-600 font-medium">
+            Contact your administrator
+          </span>
         </p>
       </div>
     </div>
